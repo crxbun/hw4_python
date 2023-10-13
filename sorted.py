2d4f8995024fbb3078feb8c840f3fa28b0ac5ea7
Kathy Tran <118565023+crxbun@users.noreply.github.com>
2023-10-12 18:07:07 -0700

    Rename sort_dictionary function to reverse_sort_dictionary

--- sorted.py
+++ sorted.py
@@ -1,4 +1,4 @@
-def sort_dictionary(d: dict) -> dict:
+def reverse_sort_dictionary(d: dict) -> dict:
     solution = []
     for key in sorted(d.keys(), reverse = True):
         solution.append((key, d[key][0]))
